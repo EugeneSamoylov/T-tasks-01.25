20a9e47a657a081c46b6724b43f3eb7e289bc6ae
Samoylov_Eugene <samyelson.00@yandex.ru>
2025-01-30 23:27:11 +0300

    Task 4

--- task.js
+++ task.js
@@ -1,3 +1,4 @@
+const { NOTIMP } = require("dns");
 var readline = require("readline");
 var rl = readline.createInterface({
   input: process.stdin,
@@ -9,67 +10,76 @@ process.stdin.on("end", () => {
 });
 
 let lineOne, lineTwo;
+
 rl.on("line", function (data) {
   if (!lineOne) {
     lineOne = data.split(" ").map(Number);
   } else {
-    lineTwo = data.split(" ").map(Number);
-
-    goodSchedule(lineOne, lineTwo);
+    lineTwo = data.split(" ").map(BigInt);
+    console.log(globalMinOperations());
   }
 });
 
-function goodSchedule(lineOne, lineTwo) {
-  let left, right, n, m, arr, counter;
-  let sum = Infinity;
-
-  [n, m] = [...lineOne];
-  [left, right, ...arr] = [...lineTwo];
-  arr.sort((a, b) => a - b);
-
-  if (arr[arr.length - 1] < left) {
-    sum = left - arr[arr.length - m];
-  } else if (arr[0] > right) {
-    sum = arr[m - 1] - right;
-  } else {
-    let counterArray = arr.filter((a) => a >= left && a <= right);
-
-    counter = counterArray.length;
-    if (counter === 0) {
-      counterArray[0] = arr.find((a) => a > right);
-      counterArray[1] = arr.findLast((a) => a < left);
+function NOK(A) {
+  let n = A.length,
+    a = Math.abs(A[0]);
+  for (let i = 1; i < n; i++) {
+    let b = Math.abs(A[i]),
+      c = a;
+    while (a && b) {
+      a > b ? (a %= b) : (b %= a);
     }
-    let residue = m - counter;
-    if (residue > 0) {
-      let indexInsideFirst = arr.findIndex((a) => a === counterArray[0]);
-      let indexInsideLast = arr.findLastIndex(
-        (a) => a === counterArray[counterArray.length - 1]
-      );
-      let nearArray = arr
-        .slice(
-          indexInsideFirst - residue >= 0 ? indexInsideFirst - residue : 0,
-          indexInsideLast + residue + 1
-        )
-        .filter((a) => a < left || a > right);
-      let l = 0;
-      let r = residue - 1;
-
-      while (l <= residue && r < nearArray.length) {
-        let leftSum = left - nearArray[l] > 0 ? left - nearArray[l] : 0;
-        let rightSum = nearArray[r] - right > 0 ? nearArray[r] - right : 0;
-
-        let newSum = leftSum + rightSum;
-
-        if (newSum < sum) {
-          sum = newSum;
-        }
-        l++;
-        r++;
-      }
-    } else {
-      sum = 0;
-    }
+    a = Math.abs(c * A[i]) / (a + b);
   }
+  return a;
+}
 
-  console.log(sum);
+function minOperarionOfNum(num) {
+  num = BigInt(num);
+  let minOperation = Infinity,
+    operations;
+  lineTwo.forEach((element) => {
+    if (element <= num) {
+      operations = num - element;
+    } else {
+      let rest = element % num;
+      operations = rest < Number(num) / 2 ? rest : num - rest;
+    }
+    minOperation = minOperation < operations ? minOperation : operations;
+  });
+  return minOperation;
+}
+
+function globalMinOperations() {
+  let answerOperation;
+  let x, y, z, nokAll, nokFirstTwo, nokLastTwo;
+  let sumNokAll = Infinity,
+    sumFirst = Infinity,
+    sumLast = Infinity,
+    sumAll = Infinity;
+
+  [, x, y, z] = [...lineOne];
+
+  nokAll = NOK([x, y, z]);
+  nokFirstTwo = NOK([x, y]);
+  nokLastTwo = NOK([y, z]);
+
+  sumNokAll = minOperarionOfNum(nokAll);
+  if (sumNokAll == 0) return 0;
+  if (nokFirstTwo !== nokAll) {
+    sumFirst = minOperarionOfNum(nokFirstTwo) + minOperarionOfNum(z);
+    if (sumFirst == 0) return 0;
+  }
+  if (nokLastTwo !== nokAll) {
+    sumLast = minOperarionOfNum(nokLastTwo) + minOperarionOfNum(x);
+    if (sumLast == 0) return 0;
+  }
+  if (x !== nokAll && y !== nokAll && z !== nokAll) {
+    sumAll = minOperarionOfNum(x) + minOperarionOfNum(y) + minOperarionOfNum(z);
+    if (sumAll == 0) return 0;
+  }
+  answerOperation = [sumNokAll, sumFirst, sumLast, sumAll].reduce((a, b) =>
+    b < a ? b : a
+  );
+  return answerOperation.toString();
 }
